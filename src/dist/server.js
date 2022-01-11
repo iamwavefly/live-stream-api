5e0d109fcf2e3efd2bd90bc48b477bb99f3414d7
Olawalle <Olawalle94@gmail.com>
2022-01-11 11:56:35 +0100

    update

--- src/dist/server.js
+++ src/dist/server.js
@@ -8,14 +8,13 @@ const morgan_1 = (0, tslib_1.__importDefault)(require("morgan"));
 const docs_1 = (0, tslib_1.__importDefault)(require("./docs/"));
 const user_1 = (0, tslib_1.__importDefault)(require("./api/routes/user/"));
 const app = (0, express_1.default)();
-const port = 4000;
 // Body parsing Middleware
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use((0, morgan_1.default)("dev"));
 app.use((0, cors_1.default)());
 app.get("/", (req, res) => (0, tslib_1.__awaiter)(void 0, void 0, void 0, function* () {
-    return res.status(200).send({
+    res.status(200).send({
         message: "Live sumo API index",
     });
 }));
@@ -23,6 +22,7 @@ app.use("/api/user", user_1.default);
 // swagger setup
 app.use("/api-docs", swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(docs_1.default));
 // create port server
+const port = process.env.PORT || 5000;
 try {
     app.listen(port, () => {
         console.log(`Connected successfully on port ${port}`);
