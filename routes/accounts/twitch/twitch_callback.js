2e8a9daa45f272439a9bb93a624f422ac9bc7bbe
ukemike <ukemike4@gmail.com>
2022-03-07 16:39:05 +0100

    7/3/2022

--- routes/accounts/twitch/twitch_callback.js
+++ routes/accounts/twitch/twitch_callback.js
@@ -38,17 +38,12 @@ module.exports = function (app) {
             });
 
             const user_profile_body = user_profile_response.data.data[0];
-            
-            console.log(user_profile_body, 'user_profile_body');
 
             const user_profile_name = user_profile_body.display_name;
-            console.log(user_profile_name, 'user_profile_name');
 
             const user_profile_picture = user_profile_body.profile_image_url;
-            console.log(user_profile_picture, 'user_profile_picture');
 
             const user_profile_id = user_profile_body.id;
-            console.log(user_profile_id, 'user_profile_id');
 
             
             if (functions.empty(exchange_token_response.data)) { throw new Error("Access token and refresh token data are missing."); }
@@ -68,6 +63,7 @@ module.exports = function (app) {
                         twitch_profile_picture: user_profile_picture,
                         twitch_profile_name: user_profile_name,
                         twitch_profile_id: user_profile_id,
+                        is_connected_twitch: true
                     }
                 }, { new: true });
 
